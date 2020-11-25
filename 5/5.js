ba7ebe6695e53293409dccb69331444b6ecf64b7
Margarita <mpankratova13@gmail.com>
2020-11-25 16:40:49 +0300

    Made update example 5

--- 5/5.js
+++ 5/5.js
@@ -6,7 +6,7 @@
  * @returns {number}
  */
 function summarize(a , b) {
-    return (a + b);
+    return a + b;
 }
 
 /**Функция возвращает разницу 2ух заданных чисел
@@ -15,7 +15,7 @@ function summarize(a , b) {
  * @returns {number}
  */
 function deduction(c, d) {
-    return(c - d);
+    return c - d;
 }
 
 /**Функция возвращает деление 2ух заданных чисел
@@ -24,7 +24,7 @@ function deduction(c, d) {
  * @returns {number}
  */
 function division(a, b) {
-    return(a / b);
+    return a / b;
 }
 
 /**Функция возвращает произведение 2ух заданных чисел
@@ -33,7 +33,7 @@ function division(a, b) {
  * @returns {number}
  */
 function multiplication(c, d) {
-    return(c * d);
+    return c * d;
 }
 
 /**Функция возвращает случайное целое число от 0 до 99
